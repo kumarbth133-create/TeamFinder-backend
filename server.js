e907c64e07013628982ddbd6f6d059b35c4157f5
Kundan Gupta <kumarbth133@gmail.com>
2026-08-16 15:07:46 +0530

    server change

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "team-finder-backend.vercel.app", credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
